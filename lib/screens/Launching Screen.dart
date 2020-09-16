e8b20d486595562eab6ac342710d9f75c771bee4
Movin Silva <movinpinsara@gmail.com>
2020-09-16 11:12:34 +0530

    login page added

---
+++ lib/screens/Launching Screen.dart
@@ -0,0 +1,13 @@
+/*
+* Created by Movin on 16/09/2020
+* */
+
+
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
